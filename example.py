98f9cdc432dcaa6911db0f687df428a94c0580d8
nadahlberg <nadahlberg@gmail.com>
2021-04-29 20:23:10 -0400

    change data filenames

--- example.py
+++ example.py
@@ -1,10 +1,10 @@
 import pandas as pd
 from pipelines import pipeline
 
-data = pd.read_csv('data/dockets.csv')
+data = pd.read_csv('data/docket_text.csv')
 docs = list(data['docket_text'].values)
 
 nlp = pipeline('docket-classification')
 
 data['labels'] = nlp(docs)
-data.to_csv('data/tagged_dockets.csv', index=False)
+data.to_csv('data/tagged_docket_text.csv', index=False)
