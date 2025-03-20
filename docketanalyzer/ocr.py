e3b241ed4831d64963e69a0e66d29dfef41aaa60
Nathan Dahlberg <nadahlberg@gmail.com>
2025-03-20 02:09:25 -0400

    bump ocr

--- docketanalyzer/ocr.py
+++ docketanalyzer/ocr.py
@@ -1,9 +1,27 @@
 from . import extension_required
 
 with extension_required("ocr"):
-    from docketanalyzer_ocr import pdf_document
+    from docketanalyzer_ocr import (
+        PDFDocument,
+        box_overlap_pct,
+        bulk_process_pdfs,
+        extract_native_text,
+        extract_ocr_text,
+        load_pdf,
+        merge_boxes,
+        pdf_document,
+        predict_layout,
+    )
 
 
 __all__ = [
+    "PDFDocument",
+    "box_overlap_pct",
+    "bulk_process_pdfs",
+    "extract_native_text",
+    "extract_ocr_text",
+    "load_pdf",
+    "merge_boxes",
     "pdf_document",
+    "predict_layout",
 ]
